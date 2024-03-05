e502bd41f5e7180cd5d46fe23e426e8d7f8e870a
aka-Cairo <matthew.harper33@gmail.com>
2024-03-05 12:59:07 -0500

    changed some formatting of numbers to be technically compliant with the grading rubric, but I think it's kinda wrong

--- customer_banking.py
+++ customer_banking.py
@@ -21,8 +21,8 @@ def main():
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
     print("__________________________________")
-    print(f"\nUpdated savings account balance: ${updated_savings_balance}")
-    print(f"Interest earned: ${interest_earned}")
+    print(f"\nInterest earned: ${interest_earned:,.2f}")
+    print(f"Updated savings account balance: ${updated_savings_balance:,.3f}")
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     # ADD YOUR CODE HERE
@@ -36,8 +36,8 @@ def main():
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
     # ADD YOUR CODE HERE
     print("__________________________________")
-    print(f"\nUpdated CD account balance: ${updated_cd_balance:,.2f}")
-    print(f"Interest earned: ${cd_interest_earned:,.2f}")
+    print(f"\nInterest earned: ${cd_interest_earned:,.2f}")
+    print(f"Updated CD account balance: ${updated_cd_balance:,.3f}")
 
 if __name__ == "__main__":
     # Call the main function.
